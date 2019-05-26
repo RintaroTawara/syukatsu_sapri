2c7ddce8d5d113bb99dbab20c1211a0eb330d9bf
EC2 Default User <ec2-user@ip-172-31-26-13.us-east-2.compute.internal>
2019-05-26 21:25:04 +0000

    add username to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190526061359) do
+ActiveRecord::Schema.define(version: 20190526212035) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20190526061359) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.boolean "admin", default: false
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
