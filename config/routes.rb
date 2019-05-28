65ac44d4155ce42966928df7ef36b1497e8c1cb4
RintaroTawara <[rintarou.54@gmail.com]>
2019-05-28 12:50:54 +0000

    end creating show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
   root to: 'articles#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, :only => [:index, :show]
